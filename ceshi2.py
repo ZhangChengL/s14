377dfd8bdc5ec83f528239367b4a3897051ef5d2
ZhangChengL <zcl19940531@163.com>
2016-12-16 15:20:31 +0800

    121615

--- ceshi2.py
+++ ceshi2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author:Zhangcl
-print('1216')
+g = open('good','r',encoding='utf-8')
+good =g.read()
+print(type(good))
+good_s= eval(good)
+print(type(good_s))
+print(good_s)
