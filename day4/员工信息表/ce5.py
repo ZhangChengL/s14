276cd0d25881858391245b95301dc3e65e070721
ZhangChengL <zcl19940531@163.com>
2017-01-19 18:20:55 +0800

    0119

--- day4/员工信息表/ce5.py
+++ day4/员工信息表/ce5.py
@@ -2,7 +2,17 @@
 # -*- coding:utf-8 -*-
 # Author:Zhangcl
 f=open('staff_table.txt','r',encoding='utf-8')
-for i in f:
-    a=i.split(' ')
-    if a[-2]=='IT':
-        print(a)
+staff_table="staff_table.txt"
+staff_old =open(staff_table,'r',encoding='utf-8')
+# for i in f:
+#     a=i.split(' ')
+#     if a[-2]=='IT':
+#         print(a)
+# for i in f:
+#     print(i)
+#     if i.strip()=='':
+#         print('yes')
+#     else:
+#         print('No')
+for i in staff_old:
+    print(i)
--- day4/员工信息表/empinfo.py
+++ day4/员工信息表/empinfo.py
@@ -18,12 +18,7 @@ import time
 staff_table="staff_table.txt"
 staff_table_new="staff_table_new.txt"
 staff_table_tmp="staff_table_tmp.txt"
-# f=open(staff_table,'r',encoding='utf-8')
-# c=0
-# for i in f:
-#     print(i)
-#     c+=1
-# print(c)
+
 path_exis =os.path.exists(staff_table)
 if path_exis == False:
     exit('配置文件不存在，强制退出')
@@ -68,16 +63,7 @@ def staff_input():
         exit("请输入正确的日期")
     return name,age,phone,dept,enroll_date
 #def ddel():#删除模块
-path()
-staff_old,staff_new=file_open()
-num=input("请输入需要删除的员工id：")
-for i in staff_old:
-    if i.strip().startswith(num):
-        continue
-    staff_new.write(i)
-staff_old.close()
-staff_new.close()
-file_tmp()
+
  #   pass
 def add():#新增
     pass
@@ -87,8 +73,89 @@ def select():#查询
     pass
 
 meg='''
+===================================
 1.查询
 2.新建
 3.删除
 4.修改
+5.退出
 '''
+meg_cx='''
+====================================
+1.按员工ID查询
+2.按姓名查询
+3.按年龄查询
+4.按电话号码查询
+5.按职位查询
+6.按入职年份查询
+7.退出查询
+'''
+while True:
+    print(meg)
+    choice=input('请选择：')
+    if choice.isdigit():
+        choices=int(choice)
+        if choices==1:
+            while True:
+                print(meg_cx)
+                cx_choice=input('请选择：')
+                if cx_choice.isdigit():
+                    cx_choices=int(cx_choice)
+                    if cx_choices==6:
+                        your_find = input('请输入需要查询年份(例：2016)：')
+                        with open(staff_table,'r',encoding='utf-8') as ff:
+                            for ii in ff:
+                                a=ii.split(' ')
+                                b=a[cx_choices-1].split('-')
+                                b_year=b[0]
+                                if b_year == your_find:
+                                    print(' '.join(a))
+                    if cx_choices==7:
+                        break
+                    if cx_choices in (1,2,3,4,5):
+                        your_find=input('请输入需要查询的内容：')
+                        with open(staff_table, 'r', encoding='utf-8') as f:
+                            for i in f:
+                                a = i.split(' ')
+                                if a[cx_choices-1] == your_find:
+                                    cc=' '.join(a)
+                                    print(cc)
+                else:
+                    print('输入错误，请重新输入')
+        if choices==2:
+            path()
+            staff_old,staff_new=file_open()
+            add_name, add_age, add_phone, add_dept, add_enroll_date = staff_input()
+            print(staff_old.read())
+            for aad in staff_old:
+                staff_new.write(aad)
+                ad_list = aad.split(' ')
+                ad_num = int(ad_list[0]) + 1
+                if ad_list[3] ==add_phone:
+                    print('电话号码重复，请确认电话号码是否正确！')
+                    break
+            add_list = [str(ad_num), add_name, add_age, add_phone, add_dept,add_enroll_date]
+            print(add_list)
+            add_staff_into = ' '.join(add_list)+'\n'
+            staff_new.write(add_staff_into)
+            staff_old.close()
+            staff_new.close()
+            file_tmp()
+        if choices==3:
+            path()
+            staff_old, staff_new = file_open()
+            num = input("请输入需要删除的员工id：")
+            for i in staff_old:
+                if i.strip().startswith(num):
+                    continue
+                staff_new.write(i)
+            staff_old.close()
+            staff_new.close()
+            file_tmp()
+            #pass
+        if choices==4:
+            pass
+        if choices==5:
+            exit()
+    else:
+        print('输入错误，请重新输入')
