e3003d0dc8cf5defae7723f376ad67721b19822a
ZhangChengL <zcl19940531@163.com>
2017-03-24 14:47:51 +0800

    20170324

--- day6/Calculator.py
+++ day6/Calculator.py
@@ -14,3 +14,47 @@
 # re.search(r'\([^()]+\)',s).group()
 #
 # '(-40/5)'
+# import re
+# s = '(-60-30+10-22) +(-40/-5)'
+# a=re.search(r'\([^()]+\)',s).group()
+# print(a)
+# b=a.split('(')[1].split(')')[0]
+# print(b)
+# sz=re.split('[-+]',b) #筛选加减
+# fh=re.findall('[-+]',b) #筛选数值
+# #re.search('[*/]')
+# print(fh)
+# print(sz)
+# print(len(b))
+import re
+
+def compute(formula):
+    #formulas=formula.split('(')[1].split(')')[0]
+    formulas=formula.strip('()')
+    print(formulas)
+    sz=re.split('[-+]',formulas)
+    print(sz)
+    fh=re.findall('[-+]',formulas)
+    print(fh)
+    if len(sz[0].strip())==0:
+        sz[1]=fh[0]+sz[1]
+        del sz[0]
+        del fh[0]
+        print(sz)
+        print(fh)
+    for index,i in enumerate(sz):
+        i = i.strip()
+        if i.endswith('*') or i.endswith('/'):
+
+    return formulas
+def calc(formula):
+    brackets_is=True
+    while brackets_is:
+        bk=re.search(r'\([^()]+\)',formula)
+        if bk:
+            formula=formula.replace(bk.group(),str(compute(bk.group())))
+        else:
+            compute(formula)
+            brackets_is=False
+
+cc=calc('(-60-30+10-22) +(-40/-5)')
